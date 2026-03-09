962c38931a1ec54bcf3af9fca4014b69574cdb6c
ahmedmahmud <ahmedmahmud12971@gmail.com>
2026-03-09 15:14:08 +0000

    fix: railway build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
-import { devtools } from "@tanstack/devtools-vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
+import { devtools } from "@tanstack/devtools-vite";
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [
     tsconfigPaths({ projects: ["./tsconfig.json"] }),
     tanstackStart(),
